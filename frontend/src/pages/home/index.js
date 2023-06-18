e97b853d9de3407b252718bb27a599979b21a478
Samuel <samur489@gmail.com>
2023-06-18 15:28:51 -0300

    modificando e criando pastas iniciais e home page

---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,22 @@
+import "./style.css"
+
+const Home = () => {
+    return (
+        <div class="root">
+            <header>PAGINA INICIAL</header>
+            <div class="buttons-wrapper">
+                <div class="button-box">
+                    <p>RECEITAS</p>
+                </div>
+                <div class="button-box">
+                    <p>INGREDIENTES</p>
+                </div>
+                <div class="button-box">
+                    <p>CONFIGURAÇÕES</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
